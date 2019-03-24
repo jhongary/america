c3707bc4e86a8545def93df5917a072774bc4e66
gary <garymarin6@gmail.com>
2019-03-24 13:07:50 -0400

    comit del model auto y persona

--- app/Model/Auto.php
+++ app/Model/Auto.php
@@ -1,12 +1,81 @@
 <?php
 App::uses('AppModel', 'Model');
 /**
- * MAterile Model
+ * Auto Model
  *
+ * @property User $User
+ * @property Persona $Persona
+ * @property Persona $Persona
+ * @property Persona $Persona
  */
 class Auto extends AppModel {
 
-	public $hasMany = 'Persona';
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Persona' => array(
+			'className' => 'Persona',
+			'foreignKey' => 'persona_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Persona' => array(
+			'className' => 'Persona',
+			'foreignKey' => 'auto_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
 
 
-	}
+/**
+ * hasAndBelongsToMany associations
+ *
+ * @var array
+ */
+	public $hasAndBelongsToMany = array(
+		'Persona' => array(
+			'className' => 'Persona',
+			'joinTable' => 'autos_personas',
+			'foreignKey' => 'auto_id',
+			'associationForeignKey' => 'persona_id',
+			'unique' => 'keepExisting',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+		)
+	);
+
+}
